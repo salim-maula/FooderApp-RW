e877157ef48b172949f628cad9ad3df18ecf9231
salim-maula <salim-maula>
2022-05-10 21:53:40 +0700

    first commit

---
+++ lib/screens/grocery_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
+import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO 2: Replace with EmptyGroceryScreen
+    return Scaffold(
+      // 6
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GroceryItemScreen(
+                onCreate: (item) {
+                  manager.addItem(item);
+                  Navigator.pop(context);
+                },
+                onUpdate: (item) {},
+              ),
+            ),
+          );
+        },
+      ),
+      // 7
+      body: buildGroceryScreen(),
+    );
+  }
+
+  Widget buildGroceryScreen() {
+    // 1
+    return Consumer<GroceryManager>(
+      // 2
+      builder: (context, manager, child) {
+        // 3
+        if (manager.groceryItems.isNotEmpty) {
+          return GroceryListScreen(manager: manager);
+        } else {
+          // 4
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
+}
